4ef27471d9c3dde04e62aacc5fd44630087870e3
johnmirby <jmi8fs@virginia.edu>
2015-09-06 03:48:25 -0400

    Fixed location update button

--- app/src/main/java/com/virginia/cs/cs4720androidproject/MainActivity.java
+++ app/src/main/java/com/virginia/cs/cs4720androidproject/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends Activity implements LocationListener{
     public void onLocationChanged(Location location) {
 
     }
+
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
 
