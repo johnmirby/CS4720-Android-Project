801f524897671c1848c1e71cf11567f77a35d640
johnmirby <jmi8fs@virginia.edu>
2015-09-06 02:30:24 -0400

    Initial commit

---
+++ app/src/main/java/com/virginia/cs/cs4720androidproject/MainActivity.java
@@ -0,0 +1,51 @@
+package com.virginia.cs.cs4720androidproject;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getGPSCoordinates(View view) {
+        EditText gpsEditText = (EditText)findViewById(R.id.gps_editText);
+        Location location= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        String gpsCoordinates = "";
+        gpsEditText.setText(gpsCoordinates);
+    }
+}
